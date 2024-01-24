ae3e6e582cf68d40c9f6554413768f5f9f3c8639
Lukas Grossmann <lgrossma@redhat.com>
2024-01-24 08:59:42 +0100

    Trying to delete kubeconfig before tests start
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>

--- test/ui/public-ui-test.ts
+++ test/ui/public-ui-test.ts
@@ -5,6 +5,7 @@
 
 import { checkAboutCommand } from './suite/command-about';
 import path = require('path');
+import * as fs from 'fs-extra';
 import { testCreateComponent } from './suite/createComponent';
 import { testDevfileRegistries } from './suite/devfileRegistries';
 import { checkExtension } from './suite/extension';
@@ -16,6 +17,23 @@ require('source-map-support').install();
 
 describe('Extension public-facing UI tests', function() {
     const contextFolder = path.join(__dirname, 'context');
+    const kubeConfig = path.join(process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'], '.kube', 'config');
+    const kubeBackup = `${kubeConfig}.backup`;
+
+    // test with an empty kube config, make a backup, wipe the context folder
+    before(async function () {
+        if (fs.existsSync(kubeConfig)) {
+            await fs.move(kubeConfig, kubeBackup, { overwrite: true });
+        }
+        await fs.emptyDir(contextFolder);
+    });
+
+    // restore the kube config backup after test
+    after(async function () {
+        if (fs.existsSync(kubeBackup)) {
+            await fs.move(kubeBackup, kubeConfig, { overwrite: true });
+        }
+    });
 
     checkExtension();
     checkOpenshiftView();
