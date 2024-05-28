cfeeace8639c044f6ccb2f902eae089b377a6215
Lukas Grossmann <lgrossma@redhat.com>
2024-05-28 16:44:20 +0200

    add refresh wait
    Signed-off-by: Lukas Grossmann <lgrossma@redhat.com>

--- test/ui/suite/createComponent.ts
+++ test/ui/suite/createComponent.ts
@@ -168,6 +168,7 @@ export function testCreateComponent(path: string) {
             console.log('5')
             await prompt.confirm();
             console.log('6')
+            await new Promise((res) => {setTimeout(res, 1_000)});
             prompt = await new Workbench().openCommandPrompt();
             console.log('7')
             await prompt.setText('>Workspaces: Remove Folder From Workspace...');
